710b231772302a1a49d1f81b9b8b7137b7b7094f
animalCodes <email@no.com>
2022-10-01 11:47:47 +0100

    update addNode to have title and content parameters with default values, rename 'body' fields to 'content'

--- node_manager.js
+++ node_manager.js
@@ -1,7 +1,7 @@
 // Grab holder from DOM
 const holder = document.getElementById("field_holder")
 // Note HTML
-const noteHtml = "<fieldset id='{{id}}'><button onclick='deleteNode({{id}})'>X</button><input type='text' id='{{id}}_title' placeholder='Title'><br/><textarea id='{{id}}_body' cols='25' rows='6'>Body</textarea></fieldset>"
+const noteHtml = "<fieldset id='{{id}}'><button onclick='deleteNode({{id}})'>X</button><input type='text' value='{{title}}'><br/><textarea cols='25' rows='6'>{{content}}</textarea></fieldset>"
 
 let nodeCount = 0; 
 let nodeBuffer = [];
@@ -18,30 +18,32 @@ function updateNodeBuffer() {
   // for every node
   for (let i = 0; i < holder.children.length; i++) {
     const child = holder.children[i];
-    // push title and body
+    // push title and content
     nodeBuffer.push([child.children[1].value, child.children[3].value]);
   }
 }
 
 /**
  * Creates a new node
+ * @param {string} title - title of note, default "Title"
+ * @param {string} content - content of note, default "Content"
  */
-function addNode() {
+function addNode(title="Title", content="Content") {
   nodeCount++;
   // * Updating the content of the holder will reset any input values, so we need to save them first.
 
   // update buffer
   updateNodeBuffer()
 
-  // add new node
-  holder.innerHTML += noteHtml.replaceAll("{{id}}", nodeCount)
+  // add new node - ugly but it works.
+  holder.innerHTML += noteHtml.replaceAll("{{id}}", nodeCount).replaceAll("{{title}}", title).replaceAll("{{content}}", content)
 
   // * Restore values
   for (let i = 0; i < holder.children.length-1/* skip just added node*/; i++) {
     const child = holder.children[i];
     // restore title
     child.children[1].value = nodeBuffer[i][0];
-    // restore body
+    // restore content
     child.children[3].value = nodeBuffer[i][1];
   }
   updateUrlDisplay()
