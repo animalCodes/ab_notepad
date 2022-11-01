981a4952b8bb9f5fc0378b646ad1a0cd9729420b
animalCodes <email@no.com>
2022-11-01 18:47:59 +0000

    cleanup

--- display_manager.js
+++ display_manager.js
@@ -1,7 +1,3 @@
-// Get URL display element from DOM
-const urlDisplay = document.getElementById("url_holder")
-const holder = document.getElementById("field_holder")
-
 // * Once page has finished loading, take a peek at the URL and convert any GET parameters into notes
 addEventListener("DOMContentLoaded", () => {
   // retrieve parameters
@@ -10,13 +6,13 @@ addEventListener("DOMContentLoaded", () => {
   // if searchParams is empty, return out.
   if (parameters.toString() == "") return;
 
-  // otherwise, delete default node.
-  deleteNode(0)
+  // otherwise, delete default note.
+  deleteNote(0)
   
   // fill out notes
   for (let i = 0; i < Number(parameters.get("count"))||0; i++) {
     // retrieve title and content, add note with retrieved data
-    addNode(parameters.get(`${i}_title`), parameters.get(`${i}_content`))
+    addNote(parameters.get(`${i}_title`), parameters.get(`${i}_content`))
   }
 
   // Update display
@@ -26,34 +22,34 @@ addEventListener("DOMContentLoaded", () => {
 // * Notes
 
 // Note HTML - I know using <div>s is unsemantic but <fieldset>s have max-width + a 2px groove border by default and I want the page to look as good as possible without any css
-const noteHtml = "<div class='note' id='note_{{id}}'><button onclick='deleteNode({{id}})'>Delete</button>\n<input type='text' value='{{title}}'><br/><textarea cols='29' rows='5'>{{content}}</textarea></div>"
+const noteHtml = "<div class='note' id='note_{{id}}'><button onclick='deleteNote({{id}})'>Delete</button>\n<input type='text' value='{{title}}'><br/><textarea cols='29' rows='5'>{{content}}</textarea></div>"
 
-let nodeCount = 1;
-let nodeBuffer = [];
+let noteCount = 1;
 
 /**
- * Creates a new node
+ * Creates a new note
  * @param {string} title - title of note, default "Title"
  * @param {string} content - content of note, default "Content"
  */
-function addNode(title="Title", content="Content") {
+function addNote(title="Title", content="Content") {
   // * Updating the content of the holder will reset any input values, so we need to save them first.
   
-  // update buffer
-  updateNodeBuffer()
-  nodeCount++;
-  
-  // add new node - ugly but it works.
-  holder.innerHTML += noteHtml.replaceAll("{{id}}", nodeCount).replaceAll("{{title}}", title).replaceAll("{{content}}", content)
+  // add new note - ugly but it works.
+  holder.innerHTML += noteHtml.replaceAll("{{id}}", noteCount).replaceAll("{{title}}", title).replaceAll("{{content}}", content)
 
+  const noteBuffer = getNoteBuffer();
+  
   // * Restore values
-  for (let i = 0; i < holder.children.length-1/* skip just added node*/; i++) {
+  for (let i = 0; i < holder.children.length-1/* skip just added note*/; i++) {
     const child = holder.children[i];
     // restore title
-    child.children[1].value = nodeBuffer[i][0];
+    child.children[1].value = noteBuffer[i][0];
     // restore content
-    child.children[2].value = nodeBuffer[i][1];
+    child.children[2].value = noteBuffer[i][1];
   }
+  
+  noteCount++;
+
   updateUrlDisplay()
 }
 
